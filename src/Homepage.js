95f0bf5620858d5d15f16a4e44a2bccb14324ad1
A2ITNotes <muadh117a@gmail.com>
2023-07-25 11:57:45 +0100

    Update Homepage.js

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,7 +5,7 @@ function Homepage(){
     return(
         <div className="">
             <h1 className="home-text">HI THERE👋,</h1>
-            <h1 className="home-text">I'M <MyComponent value="MUADH"/></h1>
+            <h1 className="home-text">I'M <MyComponent value="MUADH'S"/></h1>
             <h1 className="home-text"> FIRST REACT APP</h1>
         </div>
     )
